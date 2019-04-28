295fa36308491161085b396e7767f8f6d5b80013
zoujun <18795905365@163.com>
2019-04-28 23:58:00 +0800

    毒客第一次代码提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class=""> 
+    <h2>毒客后台管理系统</h2>
+    <a-button type="primary">hello，毒牙君</a-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue';
+export default {
+  name: 'HelloWorld',
+  components: {
+      AButton: Button
+  },
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
