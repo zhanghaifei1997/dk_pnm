27315c28b48ad25126855e791504e59ae40528c0
zoujun <18795905365@163.com>
2019-04-30 00:36:17 +0800

    登陆页面

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+
+import './assets/css/reset'
+import './assets/css/common'
+
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
-    {
+    { // 默认进入登陆页面
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: () => import("@/views/login")
     }
   ]
 })
