a242b41a481bb32ec06abca3362c0d9ae3563f13
duYa <18795905365@163.com>
2019-05-03 23:40:51 +0800

    store

--- src/components/Layout/modules/DKHeader.vue
+++ src/components/Layout/modules/DKHeader.vue
@@ -3,8 +3,25 @@
     <a-icon
       class="trigger"
       :type="$store.getters.CommonStatus.collapsed ? 'menu-unfold' : 'menu-fold'"
-      @click="change"
-    />
+      @click="change"/>
+    <div>
+      <a-dropdown>
+        <a-avatar shape="square" size="large" :style="{backgroundColor: color, verticalAlign: 'middle'}">{{avatarValue}}</a-avatar>
+        <a class="ant-dropdown-link" href="#">
+          Click me <a-icon type="down" />
+        </a>
+        <a-menu slot="overlay">
+          <a-menu-item key="0">
+            <a href="http://www.alipay.com/">1st menu item</a>
+          </a-menu-item>
+          <a-menu-item key="1">
+            <a href="http://www.taobao.com/">2nd menu item</a>
+          </a-menu-item>
+          <a-menu-divider />
+          <a-menu-item key="3">3rd menu item</a-menu-item>
+        </a-menu>
+      </a-dropdown>
+    </div>
   </a-layout-header>
 </template>
 
@@ -15,7 +32,8 @@
         },
         data() {
           return {
-
+            avatarValue: "duYa",
+            color: "#7265e6"
           }
         },
         methods: {
@@ -34,6 +52,9 @@
 
 <style lang="less" scoped>
 .dk_header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 0 20px;
   border-left: 1px solid #efefef;
   background: #fff!important;
--- src/router/permission.js
+++ src/router/permission.js
@@ -1,4 +1,9 @@
 /***
  * @author duya
  * @date 2019/04/29
+ * @desc 路由权限控制页面
  */
+
+export default {
+
+}
